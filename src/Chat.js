0eea6d765bacb59c2342d207cc1ae267937ce306
Nordwi <amosh.work@yandex.ru>
2023-12-27 18:54:28 +0500

    Добавил раздел статусов в чате и компонент уведомлений

--- src/Chat.js
+++ src/Chat.js
@@ -2,6 +2,10 @@ import "./css/custom/Chat.css";
 import "./css/bootstrap/bootstrap.css";
 import Navbar from "./componets/Navbar";
 import telegram_logo from './img/telegram.svg';
+import send from './img/Send.svg';
+import more from './img/more.png';
+import supp_icon from './img/Support.svg';
+import Notification from "./componets/Notification";
 
 
 function Chat() {
@@ -30,37 +34,60 @@ function Chat() {
                                 <div className="message col-6">
                                     <div className="user-info d-flex justify-content-between">
                                         <img className="from-logo" src={telegram_logo} alt="лого"/>
-                                        <h3 className="username">Черчиль</h3>
+                                        <h3 className="username">Имя клиента</h3>
                                         <h4 className="message-time">19:37</h4>
                                     </div>
                                     <p className="message-content">Сообщение клиента</p>
                                 </div>
                                 <div className="answer col-6">
                                     <div className="user-info d-flex justify-content-between">
-                                        <img className="from-logo" src={telegram_logo} alt="лого"/>
-                                        <h3 className="username">Черчиль</h3>
+                                        <img className="supp-logo" src={supp_icon} alt="лого"/>
+                                        <h3 className="username">Имя сотрудника</h3>
                                         <h4 className="message-time">19:37</h4>
                                     </div>
-                                    <p className="message-content">Сообщение клиента</p>
+                                    <p className="message-content">Сообщение сотрудника</p>
                                 </div>
                                 <div className="message col-6">
                                     <div className="user-info d-flex justify-content-between">
                                         <img className="from-logo" src={telegram_logo} alt="лого"/>
-                                        <h3 className="username">Черчиль</h3>
+                                        <h3 className="username">Имя клиента</h3>
                                         <h4 className="message-time">19:37</h4>
                                     </div>
                                     <p className="message-content">Сообщение клиента</p>
                                 </div>
                             </div> 
                         </div>
-                        <div className="input-block d-flex justify-content-center col-10">
+                        <div className="input-message d-flex justify-content-center col-10">
                             <input className="message-write w-100" type="text" placeholder="Написать сообщение"></input>
-                            <button className="send-btn" type="submit"></button>
-                        </div>  
+                            <button className="send-btn" type="submit"><img className="send" src={send}/></button>
+                            <input className="input-more" type="file" accept="image/*"></input>
+                            
+                        </div>
+                        
+                        
                     </div>
                 </div>
             </div>
-            <div className="col-3 d-flex flex-column main-content h-100"></div>
+            <div className="col-3 d-flex flex-column main-content h-100">
+                <div className="action-container d-flex">
+                    <div className="action-window col-9">
+                        <div className="action-header">
+                            <h2 className="action-info">История заявки</h2>
+                            <div className="action-status">
+                                <h2 className="request-status">Пришла заявка</h2>
+                                <h4 className="status-date">17.09.2023 00:00</h4>
+                            </div>
+                            <div className="action-status">
+                                <h2 className="request-status">Принята в работу</h2>
+                                <h4 className="status-date">17.09.2023 00:00</h4>
+                            </div>
+                            <button className="add-action">Добавить действие</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Notification />
+            
                 
                 
               
