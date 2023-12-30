1d8d0ff0bbdfeb5c14a75b766b1c0d3c3112f676
Vitaliy Popov <0702vitaly@gmail.com>
2023-12-30 13:15:51 +0500

    Обновление ссылок

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -5,28 +5,29 @@ import message from './../img/Message.svg';
 import test from './../img/Test.svg';
 import archive from './../img/archive.svg';
 import graph from './../img/Graph.svg';
+import {Link} from "react-router-dom";
 
 function Navbar() {
     return (
         <div className="col-2 d-flex flex-column align-items-center px-3 position-fixed vh-100" id="side_nav">
                 <img className="img-fluid mt-5" src={logo} id="logo" alt="Жизнь март"/>
                 <div className="nav-elements w-100 d-flex flex-column">
-                    <a href="#" className="nav-item">
+                    <Link to="/chat" className="nav-item">
                         <img src={message}/>
                         Чат
-                    </a>
-                    <a href="#" className="nav-item active">
+                    </Link>
+                    <Link to="/requests" className="nav-item">
                         <img src={test}/>
                         Список заявок
-                    </a>
-                    <a href="#" className="nav-item">
+                    </Link>
+                    <Link to="#" className="nav-item">
                         <img src={archive}/>
                         Архив заявок
-                    </a>
-                    <a href="#" className="nav-item">
+                    </Link>
+                    <Link to="#" className="nav-item">
                         <img src={graph}/>
                         Аналитика
-                    </a>
+                    </Link>
                 </div>  
         </div>
     )
